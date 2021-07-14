fc34fff13fcb30af073a79aaf40fbc66f9d79c97
Ravi <dahalravindra@gmail.com>
2021-07-15 01:33:40 +0545

    Added the services sectioni

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./_services.scss";
+import { GiCheckMark } from "react-icons/gi";
 
 const Services = () => {
   return (
-    <section className="services half-container">
-      <h1 className="services__header">PICK A SERVICE TO VIEW ITS PRICING</h1>
+    <section className="servicess half-container">
+      <h1 className="services__header">PICK A SERVICE TO VIEW ITS PRICING</h1>{" "}
+      <br />
       <div className="services__container">
         <h4 className="services__container__content">
           SEARCH ENGINE OPTIMIZATION
@@ -15,16 +17,226 @@ const Services = () => {
       </div>
       <center>
         <h1 className="services__second-header">SEARCH ENGINE OPTIMIZATION</h1>
-        <p className="services__sub-header">Prices Starting At $%95/MO</p>
-
+        <p className="services__sub-header">Prices Starting At $595/MO</p>
         <div className="services__cards">
-          <div className="card__header">
+          <div className="services__cards__card">
+            <div className="services__cards__card__header">
+              <center>
+                <h3 className="services__cards__card__header__text">
+                  GALACTIC
+                </h3>
+                <h1 className="services__cards__card__header__money">
+                  <span>$</span> 1295 <span>/mo</span>
+                </h1>
+                <button className="services__cards__card__header__learn-more">
+                  LEARN MORE
+                </button>
+              </center>
+            </div>
             <center>
-              <h3 className="card__header__text">GALACTIC</h3>
-              <h1 className="card__header__money">
-                <span>$</span> 1295 <span>/mo</span>
-              </h1>
-              <button className="card__header__learn-more">LEARN MORE</button>
+              <ul className="services__cards__card__body">
+                <li className="services__cards__card__body__li">
+                  <GiCheckMark />
+                  <p className="services__cards__card__body__li__p">
+                    50 Keywords
+                  </p>
+                </li>
+                <li className="services__cards__card__body__li">
+                  <GiCheckMark />
+                  <p className="services__cards__card__body__li__p">
+                    1 Call to action landing page
+                  </p>
+                </li>
+                <li className="services__cards__card__body__li">
+                  <GiCheckMark />
+                  <p className="services__cards__card__body__li__p">
+                    Monthly performance reporting
+                  </p>
+                </li>
+                <li className="services__cards__card__body__li">
+                  <GiCheckMark />
+                  <p className="services__cards__card__body__li__p">
+                    High quality backlinks
+                  </p>
+                </li>
+                <li className="services__cards__card__body__li">
+                  <GiCheckMark />
+                  <p className="services__cards__card__body__li__p">
+                    1/mo SEO Content article written
+                  </p>
+                </li>
+                <li className="services__cards__card__body__li">
+                  <GiCheckMark />
+                  <p className="services__cards__card__body__li__p">
+                    Google Ads & display ad management.
+                  </p>
+                </li>
+              </ul>
+            </center>
+          </div>
+          <div className="services__cards__card">
+            <div className="services__cards__card__header">
+              <center>
+                <h3 className="services__cards__card__header__text">
+                  GALACTIC
+                </h3>
+                <h1 className="services__cards__card__header__money">
+                  <span>$</span> 1295 <span>/mo</span>
+                </h1>
+                <button className="services__cards__card__header__learn-more">
+                  LEARN MORE
+                </button>
+              </center>
+            </div>
+            <center>
+              <ul className="services__cards__card__body">
+                <li className="services__cards__card__body__li">
+                  <GiCheckMark />
+                  <p className="services__cards__card__body__li__p">
+                    50 Keywords
+                  </p>
+                </li>
+                <li className="services__cards__card__body__li">
+                  <GiCheckMark />
+                  <p className="services__cards__card__body__li__p">
+                    1 Call to action landing page
+                  </p>
+                </li>
+                <li className="services__cards__card__body__li">
+                  <GiCheckMark />
+                  <p className="services__cards__card__body__li__p">
+                    Monthly performance reporting
+                  </p>
+                </li>
+                <li className="services__cards__card__body__li">
+                  <GiCheckMark />
+                  <p className="services__cards__card__body__li__p">
+                    High quality backlinks
+                  </p>
+                </li>
+                <li className="services__cards__card__body__li">
+                  <GiCheckMark />
+                  <p className="services__cards__card__body__li__p">
+                    1/mo SEO Content article written
+                  </p>
+                </li>
+                <li className="services__cards__card__body__li">
+                  <GiCheckMark />
+                  <p className="services__cards__card__body__li__p">
+                    Google Ads & display ad management.
+                  </p>
+                </li>
+              </ul>
+            </center>
+          </div>
+          <div className="services__cards__card">
+            <div className="services__cards__card__header">
+              <center>
+                <h3 className="services__cards__card__header__text">
+                  GALACTIC
+                </h3>
+                <h1 className="services__cards__card__header__money">
+                  <span>$</span> 1295 <span>/mo</span>
+                </h1>
+                <button className="services__cards__card__header__learn-more">
+                  LEARN MORE
+                </button>
+              </center>
+            </div>
+            <center>
+              <ul className="services__cards__card__body">
+                <li className="services__cards__card__body__li">
+                  <GiCheckMark />
+                  <p className="services__cards__card__body__li__p">
+                    50 Keywords
+                  </p>
+                </li>
+                <li className="services__cards__card__body__li">
+                  <GiCheckMark />
+                  <p className="services__cards__card__body__li__p">
+                    1 Call to action landing page
+                  </p>
+                </li>
+                <li className="services__cards__card__body__li">
+                  <GiCheckMark />
+                  <p className="services__cards__card__body__li__p">
+                    Monthly performance reporting
+                  </p>
+                </li>
+                <li className="services__cards__card__body__li">
+                  <GiCheckMark />
+                  <p className="services__cards__card__body__li__p">
+                    High quality backlinks
+                  </p>
+                </li>
+                <li className="services__cards__card__body__li">
+                  <GiCheckMark />
+                  <p className="services__cards__card__body__li__p">
+                    1/mo SEO Content article written
+                  </p>
+                </li>
+                <li className="services__cards__card__body__li">
+                  <GiCheckMark />
+                  <p className="services__cards__card__body__li__p">
+                    Google Ads & display ad management.
+                  </p>
+                </li>
+              </ul>
+            </center>
+          </div>
+          <div className="services__cards__card">
+            <div className="services__cards__card__header">
+              <center>
+                <h3 className="services__cards__card__header__text">
+                  GALACTIC
+                </h3>
+                <h1 className="services__cards__card__header__money">
+                  <span>$</span> 1295 <span>/mo</span>
+                </h1>
+                <button className="services__cards__card__header__learn-more">
+                  LEARN MORE
+                </button>
+              </center>
+            </div>
+            <center>
+              <ul className="services__cards__card__body">
+                <li className="services__cards__card__body__li">
+                  <GiCheckMark />
+                  <p className="services__cards__card__body__li__p">
+                    50 Keywords
+                  </p>
+                </li>
+                <li className="services__cards__card__body__li">
+                  <GiCheckMark />
+                  <p className="services__cards__card__body__li__p">
+                    1 Call to action landing page
+                  </p>
+                </li>
+                <li className="services__cards__card__body__li">
+                  <GiCheckMark />
+                  <p className="services__cards__card__body__li__p">
+                    Monthly performance reporting
+                  </p>
+                </li>
+                <li className="services__cards__card__body__li">
+                  <GiCheckMark />
+                  <p className="services__cards__card__body__li__p">
+                    High quality backlinks
+                  </p>
+                </li>
+                <li className="services__cards__card__body__li">
+                  <GiCheckMark />
+                  <p className="services__cards__card__body__li__p">
+                    1/mo SEO Content article written
+                  </p>
+                </li>
+                <li className="services__cards__card__body__li">
+                  <GiCheckMark />
+                  <p className="services__cards__card__body__li__p">
+                    Google Ads & display ad management.
+                  </p>
+                </li>
+              </ul>
             </center>
           </div>
         </div>
